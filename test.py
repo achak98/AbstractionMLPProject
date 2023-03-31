8625c10d4f6243cd31b82ad70c41c44d9e942cf0
Abhirup Chakravarty <ac@Abhirups-MacBook-Air.local>
2023-03-31 07:08:23 +0100

    idk

--- test.py
+++ test.py
@@ -495,7 +495,7 @@ def main():
     df_test_trimmed = df_test[['article', 'highlights']]
     df_validation_trimmed = df_validation[['article', 'highlights']]
     
-    df_test_trimmed = df_test_trimmed[:4]
+    df_test_trimmed = df_test_trimmed[:8]
     #df_test_trimmed = pd.read_csv('CNN DailyMail Summarisation Data/test_stopwords.csv', encoding = "latin-1")
     #df_train_trimmed = pd.read_csv('CNN DailyMail Summarisation Data/train_stopwords.csv', encoding = "latin-1")
     #df_validation_trimmed = pd.read_csv('CNN DailyMail Summarisation Data/validation_stopwords.csv', encoding = "latin-1")
